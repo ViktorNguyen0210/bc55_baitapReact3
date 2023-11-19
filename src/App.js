f53b00c085def2e4fbb068ecf1e76d5abb53b2a3
viktornguyen0210 <nhv202@gmail.com>
2023-11-19 23:24:59 +0700

    upload exercises

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ShoesStore from "./ShoesStore/shoesStore.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <ShoesStore />
+    </div>
+  );
+}
+
+export default App;
